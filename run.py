c7430a6eb63fb575bb764fd0cb621fe3a5e245e7
Henry <hglasspool@googlemail.com>
2023-12-29 10:58:00 +0000

    added stock data retrieval function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -51,6 +52,19 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+
+    print("Calculating stock value...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to love sandwiches data automation")
+main()
